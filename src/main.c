bf9e058d9a070ffc8271d28dfd5f2835f4510c92
RTBlanco <ronnytoribio1@hotmail.com>
2026-06-07 22:41:03 -0400

    concated info

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
+#include <sys/stat.h>
 
 
 /*
@@ -20,6 +21,8 @@ int main(void) {
 
   struct dirent *entry;
 
+  
+  char path[] = "/Users/ronnytoribio/Desktop/resumes/";
   folder = opendir("/Users/ronnytoribio/Downloads");
   
   if (folder == NULL) {
@@ -33,12 +36,24 @@ int main(void) {
 
       int length_of_filename = strlen(entry->d_name);
 
+      char folder_name[length_of_filename + 1];
+      int j = 0;
+
       for(int i = 0; i < length_of_filename; i++) {
-        if ((length_of_filename - 5 ) >= i & i >= 21) {
-          printf("%c", entry->d_name[i]);
+        if ((length_of_filename - 5 ) >= i && i >= 21) {
+          // printf("%c", entry->d_name[i]);
+
+          folder_name[j++] = entry->d_name[i];
         }
       }
-      printf("\n");
+
+      folder_name[j] = '\0';
+      char path_to_folder[(sizeof(folder_name) + sizeof(path))];
+
+      snprintf(path_to_folder, sizeof(path_to_folder), "%s%s", path, folder_name);
+
+      printf("%s\n", path_to_folder);
+      // printf("\n");
       // printf("%s\n", entry->d_name);
     }
   }
